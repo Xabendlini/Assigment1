3944fdee12bf25395e40ccd25d6411e9e1d680a1
Xabendlini <151826333+Xabendlini@users.noreply.github.com>
2023-12-08 10:50:09 +0200

    first commit

---
+++ Assigment1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assigment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 99; i > 1; i--)
+            {
+                Console.WriteLine($"{(i)} bottles of beer on the wall,");
+                Console.WriteLine($"{(i)} bottles of beer.");
+                Console.WriteLine("Take one down, pass it around,");
+
+            }
+
+            for (int i = 1; i > 0; i--)
+            {
+                Console.WriteLine($"{(i)}bottle of beer on the wall,");
+                Console.WriteLine($"{(i)}bottle of beer.");
+            }
+        }
+    }
+}
